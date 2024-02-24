d2a23a39f45f2c799932031d7bc8ae6f31c39a1d
undefinedCudaCore <61139751+undefinedCudaCore@users.noreply.github.com>
2024-02-24 21:15:18 +0200

    Add START GAME page visuals

--- BrainBattle/UI/StartGamePage.cs
+++ BrainBattle/UI/StartGamePage.cs
@@ -5,9 +5,53 @@ namespace BrainBattle.UI
     internal class StartGamePage
     {
         private static string goBack;
+        private static string chosenCategory;
+        private static bool wrongCategory;
 
         public static void StartTheGame(string quit)
         {
+            LoginPage.GreetText();
+
+            Console.WriteLine("Choose one of four categories to play with:".PadLeft(10));
+            Console.WriteLine("----------------------------------------------".PadLeft(83));
+            Console.WriteLine("1 - Mathematics".PadLeft(65));
+            Console.WriteLine("2 - History".PadLeft(61));
+            Console.WriteLine("3 - Wild life".PadLeft(63));
+            Console.WriteLine("4 - Cars".PadLeft(58));
+            Console.WriteLine("----------------------------------------------".PadLeft(83));
+
+            if (wrongCategory)
+            {
+                Console.WriteLine("You must choose one category!!!");
+                wrongCategory = false;
+            }
+
+            Console.WriteLine("Type number and press ENTER:");
+            Console.WriteLine();
+
+            chosenCategory = Console.ReadLine();
+
+            switch (chosenCategory)
+            {
+                case "1":
+                    EnterToGame(chosenCategory);
+                    break;
+                case "2":
+                    EnterToGame(chosenCategory);
+                    break;
+                case "3":
+                    EnterToGame(chosenCategory);
+                    break;
+                case "4":
+                    EnterToGame(chosenCategory);
+                    break;
+                default:
+                    Console.Clear();
+                    wrongCategory = true;
+                    StartTheGame(GameData.GameRules.quitToGameMenuPressed);
+                    break;
+            }
+
             Console.WriteLine();
             Console.WriteLine(quit);
             goBack = Console.ReadLine();
@@ -22,7 +66,16 @@ namespace BrainBattle.UI
                 Console.Clear();
                 StartTheGame(GameData.GameRules.quitToGameMenuPressed);
             }
+        }
 
+        public static void EnterToGame(string category)
+        {
+            wrongCategory = true;
+
+            Console.Clear();
+            LoginPage.GreetText();
+
+            Console.WriteLine("ENTERED");
         }
     }
 }
